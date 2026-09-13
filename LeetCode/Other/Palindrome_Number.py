1849aaed4ba30ba98d4209269d5cf5bf9e7c0415
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-13 18:54:38 +0530

    Add LeetCode - Palindrome Number solution

---
+++ LeetCode/Other/Palindrome_Number.py
@@ -0,0 +1,7 @@
+# LeetCode: Palindrome Number
+# Link: https://leetcode.com/problems/palindrome-number/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
