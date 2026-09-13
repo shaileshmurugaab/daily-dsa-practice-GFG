1c391ed6b28d0cf5df00ed496de6dbbc069d466b
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-13 18:47:39 +0530

    Add LeetCode - Two Sum solution

---
+++ LeetCode/Arrays/Two_Sum.py
@@ -0,0 +1,11 @@
+# LeetCode: Two Sum
+# Link: https://leetcode.com/problems/two-sum/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
