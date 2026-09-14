6191fadaeadc5643a5c8ec96c513e349bafadb25
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-14 21:14:41 +0530

    Add LeetCode - Final Value of Variable After Performing Operations solution

---
+++ LeetCode/Arrays/Final_Value_of_Variable_After_Performing_Operations.py
@@ -0,0 +1,7 @@
+# LeetCode: Final Value of Variable After Performing Operations
+# Link: https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
