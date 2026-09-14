d749b5b3952b8e7264725226cd883e2bf8c84c83
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-14 22:39:47 +0530

    Add LeetCode - Valid Palindrome solution

---
+++ LeetCode/Strings/Valid_Palindrome.py
@@ -0,0 +1,7 @@
+# LeetCode: Valid Palindrome
+# Link: https://leetcode.com/problems/valid-palindrome/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
