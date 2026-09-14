9e637c1de683f2d0c8f96994b53dbb7d34da6be9
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-14 21:12:11 +0530

    Add LeetCode - Rectangle Overlap solution

---
+++ LeetCode/Other/Rectangle_Overlap.py
@@ -0,0 +1,7 @@
+# LeetCode: Rectangle Overlap
+# Link: https://leetcode.com/problems/rectangle-overlap/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
