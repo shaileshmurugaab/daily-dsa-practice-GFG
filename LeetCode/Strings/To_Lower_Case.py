3ec27fe9147b6768a053cfbe83f461c3c5ac9ad6
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-15 22:48:50 +0530

    Add LeetCode - To Lower Case solution

---
+++ LeetCode/Strings/To_Lower_Case.py
@@ -0,0 +1,7 @@
+# LeetCode: To Lower Case
+# Link: https://leetcode.com/problems/to-lower-case/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
