ad0b0fb1b8c1eeb11c380f55fc919d51ef9319f3
shaileshmurugaab <shaileshmurugaab@gmail.com>
2026-09-15 22:50:15 +0530

    Add LeetCode - Length of Last Word solution

---
+++ LeetCode/Strings/Length_of_Last_Word.py
@@ -0,0 +1,7 @@
+# LeetCode: Length of Last Word
+# Link: https://leetcode.com/problems/length-of-last-word/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
