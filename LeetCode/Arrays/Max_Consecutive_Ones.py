c0b243b4c18336eab09c5ff1f86508116feab7a6
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 16:16:01 +0530

    Add LeetCode - Max Consecutive Ones solution

---
+++ LeetCode/Arrays/Max_Consecutive_Ones.py
@@ -0,0 +1,7 @@
+# LeetCode: Max Consecutive Ones
+# Link: https://leetcode.com/problems/max-consecutive-ones/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
