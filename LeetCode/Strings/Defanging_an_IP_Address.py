9ca22b2eae36cba49deb9a5b91b85bea2ce6c550
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 18:36:04 +0530

    Add LeetCode - Defanging an IP Address solution

---
+++ LeetCode/Strings/Defanging_an_IP_Address.py
@@ -0,0 +1,7 @@
+# LeetCode: Defanging an IP Address
+# Link: https://leetcode.com/problems/defanging-an-ip-address/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
