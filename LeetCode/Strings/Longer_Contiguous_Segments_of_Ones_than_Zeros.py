ddc8e3aaa0a789f98a944cb441d752e89d53fa11
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 17:18:40 +0530

    Add LeetCode - Longer Contiguous Segments of Ones than Zeros solution

---
+++ LeetCode/Strings/Longer_Contiguous_Segments_of_Ones_than_Zeros.py
@@ -0,0 +1,7 @@
+# LeetCode: Longer Contiguous Segments of Ones than Zeros
+# Link: https://leetcode.com/problems/longer-contiguous-segments-of-ones-than-zeros/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
