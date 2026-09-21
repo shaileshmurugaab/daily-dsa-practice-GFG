379deaa57bf59eae58011561b669986f77e30b7a
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 17:29:46 +0530

    Add LeetCode - String to Integer (atoi) solution

---
+++ LeetCode/Strings/String_to_Integer_atoi.py
@@ -0,0 +1,7 @@
+# LeetCode: String to Integer (atoi)
+# Link: https://leetcode.com/problems/string-to-integer-atoi/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
