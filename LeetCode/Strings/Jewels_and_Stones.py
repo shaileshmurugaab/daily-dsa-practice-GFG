84000514944625643698e748821494aa78beb8dc
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 18:20:22 +0530

    Add LeetCode - Jewels and Stones solution

---
+++ LeetCode/Strings/Jewels_and_Stones.py
@@ -0,0 +1,7 @@
+# LeetCode: Jewels and Stones
+# Link: https://leetcode.com/problems/jewels-and-stones/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
