1f1e38ff5822dc99293f6ce65ff8ada42a824b21
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 13:44:46 +0530

    Add LeetCode - Maximum Subarray solution

---
+++ LeetCode/Arrays/Maximum_Subarray.py
@@ -0,0 +1,7 @@
+# LeetCode: Maximum Subarray
+# Link: https://leetcode.com/problems/maximum-subarray/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
