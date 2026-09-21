74ff83324540b4e43a8df4e8adbbad92b6f57546
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-21 17:29:21 +0530

    Add LeetCode - Reverse Integer solution

---
+++ LeetCode/Other/Reverse_Integer.py
@@ -0,0 +1,7 @@
+# LeetCode: Reverse Integer
+# Link: https://leetcode.com/problems/reverse-integer/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def reverse(self, x: int) -> int:
