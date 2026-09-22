996d1b2848692c971d1be48e9371facc8351aa72
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-22 18:32:11 +0530

    Add LeetCode - Isomorphic Strings solution

---
+++ LeetCode/Strings/Isomorphic_Strings.py
@@ -0,0 +1,7 @@
+# LeetCode: Isomorphic Strings
+# Link: https://leetcode.com/problems/isomorphic-strings/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
