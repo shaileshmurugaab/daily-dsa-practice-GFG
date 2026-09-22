079a2b2f7a1b90f177bcdaf52a1c0b20613e8250
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-22 11:59:35 +0530

    Add LeetCode - Find X Value of Array II solution

---
+++ LeetCode/Arrays/Find_X_Value_of_Array_II.py
@@ -0,0 +1,8 @@
+# LeetCode: Find X Value of Array II
+# Link: https://leetcode.com/problems/find-x-value-of-array-ii/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def resultArray(self, nums: List[int], k: int, queries: List[List[int]]) -> List
+    [int]:
