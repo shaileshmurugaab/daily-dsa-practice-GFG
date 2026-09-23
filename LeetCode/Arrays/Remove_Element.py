46f88727471a7b84484aba8f7343721aba236980
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-23 15:55:46 +0530

    Add LeetCode - Remove Element solution

---
+++ LeetCode/Arrays/Remove_Element.py
@@ -0,0 +1,7 @@
+# LeetCode: Remove Element
+# Link: https://leetcode.com/problems/remove-element/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
