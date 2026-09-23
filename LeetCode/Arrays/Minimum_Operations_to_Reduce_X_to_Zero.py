c7047ac2d01532043d18baf2944da290ff4fe087
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-23 14:59:53 +0530

    Add LeetCode - Minimum Operations to Reduce X to Zero solution

---
+++ LeetCode/Arrays/Minimum_Operations_to_Reduce_X_to_Zero.py
@@ -0,0 +1,7 @@
+# LeetCode: Minimum Operations to Reduce X to Zero
+# Link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def minOperations(self, nums: list[int], x: int) -> int:
