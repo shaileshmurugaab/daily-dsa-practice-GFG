f9bd851a4671659048970805333048d1738c725e
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-23 16:47:55 +0530

    Add LeetCode - First Unique Character in a String solution

---
+++ LeetCode/Strings/First_Unique_Character_in_a_String.py
@@ -0,0 +1,7 @@
+# LeetCode: First Unique Character in a String
+# Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
