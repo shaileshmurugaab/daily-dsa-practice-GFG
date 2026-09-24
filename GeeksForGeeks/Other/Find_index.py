50e61ed9bc8785ef6b85e8e6ae75b6e2f2521d01
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-24 22:35:28 +0530

    Add GFG - Find index solution

---
+++ GeeksForGeeks/Other/Find_index.py
@@ -0,0 +1,12 @@
+# GeeksforGeeks: Find index
+# Link: https://www.geeksforgeeks.org/problems/find-index-1614919939--145853/1
+# Language: Python3
+# Synced with CodeSync
+
+arr = tuple(map(int, input().split()))
+x = int(input())
+print(arr.index(x))
+########### Write your code below ###############
+# Print the index of x in arr
+
+########### Write your code above ###############
