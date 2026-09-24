e469eaf82b801f243422ffa31e0f5ec31712bfe0
SHAILESH MURUGAA B <shaileshmurugaab@gmail.com>
2026-09-24 12:28:33 +0530

    Add LeetCode - Smallest Index With Digit Sum Equal to Index solution

---
+++ LeetCode/Arrays/Smallest_Index_With_Digit_Sum_Equal_to_Index.py
@@ -0,0 +1,7 @@
+# LeetCode: Smallest Index With Digit Sum Equal to Index
+# Link: https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/
+# Language: Python3
+# Synced with CodeSync
+
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
